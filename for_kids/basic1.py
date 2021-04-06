e01bb0706e0f7a9b58c4477f1635bfe87c561c85
omar115 <omarhasan115@gmail.com>
2021-04-07 05:18:46 +0600

    oop try

---
+++ for_kids/basic1.py
@@ -0,0 +1,16 @@
+'''
+basic concepts:
+Object Oriented Programming: it is an approach for modeling concrete, real world things
+such as cars, and relations between things like companies and employees or students and 
+teachers etc.
+
+class: a blueprint for how something should be defined. it doesn't contain any data. 
+instance: it is an object that is built from a class and contains real world data
+
+
+'''
+
+#example of a class
+
+class Dog:
+    print('')
